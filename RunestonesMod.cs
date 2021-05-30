2e593640323d35908505fec2a8261339b4d37447
maginobi <leifsahyun@gmail.com>
2021-05-30 17:48:03 -0500

    Increment version

--- RunestonesMod.cs
+++ RunestonesMod.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace Runestones
 {
-    [BepInPlugin("Maginobi.Runestones", "Runestones", "0.8.1")]
+    [BepInPlugin("Maginobi.Runestones", "Runestones", "0.9.0")]
     [BepInProcess("valheim.exe")]
     public class RunestonesMod : BaseUnityPlugin
     {
