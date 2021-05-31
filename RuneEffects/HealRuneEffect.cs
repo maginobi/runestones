ef82ee7ea303adef1a3615d12f565fa1c3aeacaa
maginobi <leifsahyun@gmail.com>
2021-05-31 16:55:10 -0500

    Significant bugfixing is complete; incrementing version

--- RuneEffects/HealRuneEffect.cs
+++ RuneEffects/HealRuneEffect.cs
@@ -7,17 +7,17 @@ using UnityEngine;
 
 namespace Runestones.RuneEffects
 {
-    class HealRuneEffect : RuneEffect
+    public class HealRuneEffect : RuneEffect
     {
         const string projectileName = "shaman_heal_aoe";
-        const float baseHeal = 30;
+        const float baseHeal = 10;
         public HealRuneEffect()
         {
             _FlavorText = "True strength is helping others";
             _EffectText = new List<string> { "Heals others", "5m radius" };
             _QualityEffectText[RuneQuality.Ancient] = new List<string> { "+100% Heal" };
             _QualityEffectText[RuneQuality.Dark] = new List<string> { "+200% Heal" };
-            _RelativeStats = new Dictionary<string, Func<string>> { { "Heal", () => $"{baseHeal * _Effectiveness * (int)_Quality :F1}" } };
+            _RelativeStats = new Dictionary<string, Func<string>> { { "Heal", () => $"{baseHeal * _Effectiveness * ((int)_Quality+1) :F1}" } };
             targetLock = true;
             speed = CastingAnimations.CastSpeed.Medium;
         }
@@ -26,10 +26,14 @@ namespace Runestones.RuneEffects
             var aoePrefab = GameObject.Instantiate((from GameObject prefab in Resources.FindObjectsOfTypeAll<GameObject>() where prefab.name == projectileName select prefab).FirstOrDefault());
             var aoe = aoePrefab.GetComponent<Aoe>();
             aoe.m_hitEnemy = false;
-            aoe.m_damage.m_damage = -baseHeal*_Effectiveness*(int)_Quality;
+            aoe.m_hitCharacters = true;
+            aoe.m_hitProps = false;
+            aoe.m_hitOwner = true;
+            aoe.m_hitFriendly = true;
+            aoe.m_damage.m_damage = -baseHeal*_Effectiveness* ((int)_Quality + 1);
+            aoe.Setup(baseAttack.GetCharacter(), Vector3.zero, 0, null, null);
 
             var go = GameObject.Instantiate(aoePrefab, targetLocation, Quaternion.identity);
-            go.GetComponent<Aoe>().Setup(baseAttack.GetCharacter(), Vector3.zero, 0, null, null);
         }
     }
 }
